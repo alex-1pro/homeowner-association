f47e5e8990beeaf543079fbd6f3c8f3fef83450f
alex-1pro <pro1.alex.b@gmail.com>
2021-08-11 19:29:51 +0300

    adding nanoid pacjage

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,15 @@ import jsonUsers from "./data/users.json"
 import jsonMessages from "./data/messages.json"
 import jsonComments from "./data/comments.json"
 import CommentModel from "./model/CommentModel";
+import { nanoid } from "nanoid";
 function App() {
   const [users, setUsers] = useState(jsonUsers.map(plainUser => new UserModel(plainUser)));
   const [activeUser, setActiveUser] = useState();
   const [messages, setMessages] = useState(jsonMessages.map(msg => new MessageModel(msg)));
   const [allComments, setAllComments] = useState(jsonComments.map(cmt => new CommentModel(cmt)));
 
+  // const id=nanoid(6);
+  // console.log(id);
 
   function login(activeUser) {
     setActiveUser(activeUser);
