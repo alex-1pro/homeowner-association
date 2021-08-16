9101479fbb6c136ac6bd949c17548f9c5b2e28e0
alex-1pro <pro1.alex.b@gmail.com>
2021-08-16 09:41:16 +0300

    changing chart colors

--- src/components/VotingComponent/VotingComponent.jsx
+++ src/components/VotingComponent/VotingComponent.jsx
@@ -29,12 +29,14 @@ function VotingComponent({ voting, onAnswer, allVoting }) {
                     label: '# of options',
                     data: [yes, no],
                     backgroundColor: [
+                        
+                        'rgba(75, 192, 192, 0.2)',
                         'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
                     ],
                     borderColor: [
+                       
+                        'rgba(75, 192, 192, 1)',
                         'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
                     ],
                     borderWidth: 1,
                 },
